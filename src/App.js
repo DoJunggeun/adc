5c995e889183dcdda9c37dc5441576a8885e2d50
dojunggeun <gss12049@gmail.com>
2020-08-14 15:10:35 +0000

    conpleted version 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, useState } from 'react';
 import './App.css';
+import tire from './tire.png';
+import logo from './1.png';
 
 class App extends Component {
 	constructor(props) {
@@ -8,6 +10,7 @@ class App extends Component {
 			date: '',
 			acd: '',
 			dest: '용인',
+			monthDisplay: '',
 		};
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
@@ -27,7 +30,8 @@ class App extends Component {
 		const now = year + '-' + month + '-' + date;
 		this.setState({
 			date: now,
-			month: month
+			month: month,
+			monthDisplay: month
 		});
 	}
 	componentDidUpdate() {
@@ -45,8 +49,8 @@ class App extends Component {
 		if (this.state.date === '') {
 			this.setState({
 				date: now,
-				month: month
-			})
+				month: month,
+			});
 		}
 	}
 
@@ -62,35 +66,57 @@ class App extends Component {
 			return;
 		}
 		if (dest === '') {
-			alert(date.substr(5,2)+'/'+date.substr(8,2)+'의 행선지를 입력해주세요!');
+			alert(date.substr(5, 2) + '/' + date.substr(8, 2) + '의 행선지를 입력해주세요!');
 			return;
 		}
 		if (acd === 0) {
-			alert(date.substr(5,2)+'/'+date.substr(8,2)+'의 누적 주행거리를 입력해주세요!');
+			alert(date.substr(5, 2) + '/' + date.substr(8, 2) + '의 누적 주행거리를 입력해주세요!');
 			return;
 		}
-		const month = date.substr(5,2);
-		let data = JSON.parse(localStorage.getItem(month));
-		if (data === null) {
+		let data = JSON.parse(localStorage.getItem('distances'));
+		if (data == null || data == undefined || data == '') {
 			data = [[date, dest, acd]];
 		} else {
 			data = Object.values(data);
 			for (let i = 0; i < data.length; i++) {
-				if (this.state.date == data[i][0]) {
-					alert(date.substr(5,2)+'/'+date.substr(8,2)+'의 주파는 이미 입력되어 있습니다!')
+				if (this.state.date === data[i][0]) {
+					alert(
+						date.substr(5, 2) +
+							'/' +
+							date.substr(8, 2) +
+							'의 주파는 이미 입력되어 있습니다!'
+					);
 					return;
 				}
 				if (this.state.date > data[i][0]) {
-					if (data[i][2] >= acd ) {
+					if (data[i][2] >= acd) {
 						if (data[i][2] >= acd) {
-							alert(date.substr(5,2)+'/'+date.substr(8,2)+'의 누적 주행거리는'+data[i][0].substr(5,2)+'/'+data[i][0].substr(8,2)+'의 누적 주행거리보다 커야합니다! ')
+							alert(
+								date.substr(5, 2) +
+									'/' +
+									date.substr(8, 2) +
+									'의 누적 주행거리는' +
+									data[i][0].substr(5, 2) +
+									'/' +
+									data[i][0].substr(8, 2) +
+									'의 누적 주행거리보다 커야합니다! '
+							);
 							return;
-					}
+						}
 					}
 				}
 				if (this.state.date < data[i][0]) {
 					if (data[i][2] <= acd) {
-						alert(date.substr(5,2)+'/'+date.substr(8,2)+'의 누적 주행거리는'+data[i][0].substr(5,2)+'/'+data[i][0].substr(8,2)+'의 누적 주행거리보다 작아야합니다! ')
+						alert(
+							date.substr(5, 2) +
+								'/' +
+								date.substr(8, 2) +
+								'의 누적 주행거리는' +
+								data[i][0].substr(5, 2) +
+								'/' +
+								data[i][0].substr(8, 2) +
+								'의 누적 주행거리보다 작아야합니다! '
+						);
 						return;
 					}
 					data.splice(i, 0, [date, dest, acd]);
@@ -102,33 +128,58 @@ class App extends Component {
 				}
 			}
 		}
-		localStorage.setItem(month, JSON.stringify(data));
+		localStorage.setItem('distances', JSON.stringify(data));
 
-		this.setState({ date: '', dest: '용인', acd: '' });
+		this.setState({ date: '', dest: '용인', acd: '', monthDisplay:date.substr(5,2) });
 	}
 	resetData() {
-	// eslint-disable-next-line no-restricted-globals
-		if (confirm('정말 초기화 하시겠습니까? 입력한 모든 데이터가 삭제됩니다.')) {
+		// eslint-disable-next-line no-restricted-globals
+		if (confirm('정말 초기화 하시겠습니까?\n입력한 모든 데이터가 삭제됩니다.')) {
 			localStorage.clear();
 			this.setState({ date: '', dest: '용인', acd: '' });
 		} else {
 		}
 	}
+
+	welcome() {
+		if (localStorage.getItem('welcome') !== 'done') {
+			if (
+			// eslint-disable-next-line no-restricted-globals
+				confirm(
+					'8월 주파부터 사용하려면 7월 마지막 운행시 주파를 입력해주셔야 결과가 정상적으로 출력됩니다!'
+				)
+			) {
+				localStorage.setItem('welcome', 'done');
+				return true;
+			} else {
+				localStorage.setItem('welcome', 'not yet');
+				return true;
+			}
+		}
+	}
 	render() {
-		console.log(this.state)
+		{
+			setTimeout(this.welcome(), 1500);
+		}
 		return (
 			<div className="App">
-				<h2>주파 계산기</h2>
+				<div className="App-header">
+					<img src={logo} alt="logo" className="headerimage" />
+					<h2>주파 계산기</h2>
+					<img src={tire} alt="tire" className="headerimage logo" />
+				</div>
 				<button
 					style={{
-						height: 30,
+						height: 32,
 						width: 50,
-						fontSize: '0.6rem',
+						fontSize: '0.7rem',
 						position: 'absolute',
 						top: 20,
-						right: 20,
+						right: 10,
 					}}
-					onClick={() => {this.resetData();}}
+					onClick={() => {
+						this.resetData();
+					}}
 				>
 					초기화
 				</button>
@@ -160,27 +211,39 @@ class App extends Component {
 						type="button"
 						value="저장"
 						onClick={this.handleSubmit}
-						style={{ fontSize: '1rem' }}
+						style={{ textAlign: 'center', fontSize: '1rem', width: '100%' }}
 					/>
 				</div>
-				<div style={{display:'flex', width:'100%', justifyContent:'center'}}>
-						<select name="month" value={this.state.month} style={{height:30, width:'25%'}} onChange={this.handleChange}>
-							<option value="01">1월 주파</option>
-							<option value="02">2월 주파</option>
-							<option value="03">3월 주파</option>
-							<option value="04">4월 주파</option>
-							<option value="05">5월 주파</option>
-							<option value="06">6월 주파</option>
-							<option value="07">7월 주파</option>
-							<option value="08">8월 주파</option>
-							<option value="09">9월 주파</option>
-							<option value="10">10월 주파</option>
-							<option value="11">11월 주파</option>
-							<option value="12">12월 주파</option>
-						</select>
+				<div
+					style={{
+						display: 'flex',
+						width: '100%',
+						justifyContent: 'center',
+						paddingBottom: 10,
+					}}
+				>
+					<select
+						name="monthDisplay"
+						value={this.state.monthDisplay}
+						style={{ height: 30, width: '30%', fontSize: '1rem', textAlign: 'center' }}
+						onChange={this.handleChange}
+					>
+						<option value="01">1월 주파</option>
+						<option value="02">2월 주파</option>
+						<option value="03">3월 주파</option>
+						<option value="04">4월 주파</option>
+						<option value="05">5월 주파</option>
+						<option value="06">6월 주파</option>
+						<option value="07">7월 주파</option>
+						<option value="08">8월 주파</option>
+						<option value="09">9월 주파</option>
+						<option value="10">10월 주파</option>
+						<option value="11">11월 주파</option>
+						<option value="12">12월 주파</option>
+					</select>
 				</div>
 				<div className="outputs">
-					<Output month={this.state.month}/>
+					<Output month={this.state.monthDisplay} />
 				</div>
 			</div>
 		);
@@ -190,15 +253,46 @@ class App extends Component {
 export default App;
 
 function Output(props) {
+	const forceUpdate = useForceUpdate();
 	const month = props.month;
-	const lastacd = 80945;
-	let datas = JSON.parse(localStorage.getItem(month));
+	let fullDatas = JSON.parse(localStorage.getItem('distances'));
+	if (fullDatas == null || fullDatas == undefined || fullDatas == '') {
+		return (
+			<Fragment>
+				<table
+					border="1px solid black"
+					width="360"
+					align="center"
+					style={{ fontSize: '0.9rem' }}
+				>
+					<tr>
+						<th>날짜</th>
+						<th>행선지</th>
+						<th>일간/월간</th>
+						<th>누적</th>
+					</tr>
+				</table>
+			</Fragment>
+		);
+	}
+
+	let datas = [];
+	let lastacd = 0;
+	for (let i = 0; i < fullDatas.length; i++) {
+		if (fullDatas[i][0].substr(5, 2) === month) {
+			datas.push(fullDatas[i]);
+		}
+		if (fullDatas[i][0].substr(5, 2) === '0' + (month - 1)) {
+			lastacd = fullDatas[i][2];
+		}
+	}
+
 	let list;
 	let monthsum = 0;
+
 	if (datas === null) {
 		return (
 			<Fragment>
-				<h3>{month}월 주파</h3>
 				<table
 					border="1px solid black"
 					width="360"
@@ -215,13 +309,20 @@ function Output(props) {
 			</Fragment>
 		);
 	} else {
+		if (fullDatas[0][0].substr(5, 2) === month) {
+			fullDatas[0][3] = '??';
+			fullDatas[0][4] = '??';
+		}
 		for (let i = 0; i < datas.length; i++) {
+			if (datas[i][3] === '??') continue;
 			if (i === 0) {
-				datas[i].push(datas[i][2] - lastacd);
-				datas[i].push(datas[i][3]);
+				datas[i][3] = datas[i][2] - lastacd;
+				monthsum = monthsum + datas[i][3];
+				datas[i][4] = monthsum;
 			} else {
-				datas[i].push(datas[i][2] - datas[i - 1][2]);
-				datas[i].push(datas[i][3] + datas[i - 1][4]);
+				datas[i][3] = datas[i][2] - datas[i - 1][2];
+				monthsum = monthsum + datas[i][3];
+				datas[i][4] = monthsum;
 			}
 		}
 		list = datas.map((data) => (
@@ -234,13 +335,20 @@ function Output(props) {
 					{data[3]} / {data[4]}
 				</td>
 				<td>{data[2]}</td>
+				<button
+					onClick={() => {
+						deleteData(data[0]);
+						forceUpdate();
+					}}
+				>
+					del
+				</button>
 			</tr>
 		));
 	}
 
 	return (
 		<Fragment>
-			<h3>{month}월 주파</h3>
 			<table
 				border="1px solid black"
 				width="360"
@@ -269,3 +377,21 @@ function yoil(day) {
 	if (day === 6) return <span style={{ color: 'darkblue', fontWeight: 500 }}>토</span>;
 }
 
+function deleteData(date) {
+	console.log('clicked');
+	let fullDatas = JSON.parse(localStorage.getItem('distances'));
+	for (let i = 0; i < fullDatas.length; i++) {
+		console.log('for');
+		if (fullDatas[i][0] === date) {
+			console.log('if');
+			fullDatas.splice(i, 1);
+			localStorage.setItem('distances', JSON.stringify(fullDatas));
+			return;
+		}
+	}
+}
+
+function useForceUpdate() {
+	const [value, setValue] = useState(0); // integer state
+	return () => setValue((value) => ++value); // update the state to force render
+}
