39236bce4b45aebd4e301e85249ebfdbbaadbcb6
dojunggeun <gss12049@gmail.com>
2020-08-15 04:57:17 +0000

    Updates

--- precache-manifest.acb52cebe64b22b39ce83049426266ac.js
+++ precache-manifest.acb52cebe64b22b39ce83049426266ac.js
@@ -12,19 +12,19 @@ self.__precacheManifest = [
     "url": "/adc/static/js/runtime~main.e081aa2a.js"
   },
   {
-    "revision": "55b63c4e56ef198ed99a",
-    "url": "/adc/static/js/main.55b63c4e.chunk.js"
+    "revision": "e79eed8e7fc98263d8fb",
+    "url": "/adc/static/js/main.e79eed8e.chunk.js"
   },
   {
-    "revision": "e5125bdda84b7a1d06e1",
-    "url": "/adc/static/js/1.e5125bdd.chunk.js"
+    "revision": "5a3d8784d93b9cf5d37a",
+    "url": "/adc/static/js/1.5a3d8784.chunk.js"
   },
   {
-    "revision": "55b63c4e56ef198ed99a",
-    "url": "/adc/static/css/main.000a9b3e.chunk.css"
+    "revision": "e79eed8e7fc98263d8fb",
+    "url": "/adc/static/css/main.2140b86d.chunk.css"
   },
   {
-    "revision": "b18ff9f6cb527d842dd21625cf953e76",
+    "revision": "d6b8d6a07fa79a10215c5681e2e4fa9c",
     "url": "/adc/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/adc/precache-manifest.271b36644b42e954cc647a6a3e21ab9d.js"
+  "/adc/precache-manifest.acb52cebe64b22b39ce83049426266ac.js"
 );
 
 workbox.clientsClaim();
